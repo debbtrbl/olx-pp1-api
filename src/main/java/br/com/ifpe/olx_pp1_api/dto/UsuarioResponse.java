a572a9865a2957285dd5a5403d8dc0c90e4beeb5
Marcos <marcos.vinicius.taira@gmail.com>
2025-12-10 08:41:48 -0300

    get usuario atualizado

--- src/main/java/br/com/ifpe/olx_pp1_api/dto/UsuarioResponse.java
+++ src/main/java/br/com/ifpe/olx_pp1_api/dto/UsuarioResponse.java
@@ -1,26 +1,39 @@
 package br.com.ifpe.olx_pp1_api.dto;
 
 import java.util.Set;
-
+import java.time.LocalDate;
 import br.com.ifpe.olx_pp1_api.modelo.Role;
 import br.com.ifpe.olx_pp1_api.modelo.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import br.com.ifpe.olx_pp1_api.modelo.Endereco;
 
-@Data 
-@Builder 
-@NoArgsConstructor 
+@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UsuarioResponse {
-    
+
     private Long id;
     private String nome;
     private String email;
     private String cpfCnpj;
     private String telefone;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataNascimento;
+
     private String cep;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String complemento;
+
     private Set<Role> roles;
     private boolean possuiCredenciaisMercadoPago;
 
@@ -31,11 +44,16 @@ public class UsuarioResponse {
                 .email(usuario.getEmail())
                 .cpfCnpj(usuario.getCpfCnpj())
                 .telefone(usuario.getTelefone())
-                
-            
-                .cep(usuario.getEndereco() != null ? usuario.getEndereco().getCep() : null)
-    
-                
+                .dataNascimento(usuario.getDataNascimento())
+
+                .cep(endereco != null ? endereco.getCep() : null)
+                .logradouro(endereco != null ? endereco.getLogradouro() : null)
+                .numero(endereco != null ? endereco.getNumero() : null)
+                .bairro(endereco != null ? endereco.getBairro() : null)
+                .cidade(endereco != null ? endereco.getCidade() : null)
+                .uf(endereco != null ? endereco.getUf() : null)
+                .complemento(endereco != null ? endereco.getComplemento() : null)
+
                 .roles(usuario.getRoles())
                 .possuiCredenciaisMercadoPago(usuario.getMercadoPagoAccessToken() != null)
                 .build();
